a42f1ed3e13cd81a40b82a4510fad6a6179f5a2b
Casper Smits <caspersmits11@outlook.com>
2018-09-13 22:12:02 +0200

    initial commit

---
+++ src/main/java/com/iis/main/Launcher.java
@@ -0,0 +1,13 @@
+package main.java.com.iis.main;
+
+public class Launcher {
+
+    /**
+     * The launcher is responsible for starting up networks
+     *
+     * @param args arguments (empty)
+     */
+    public static void main(String[] args) {
+        
+    }
+}
