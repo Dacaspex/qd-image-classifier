9524724b5cdf6da6ebdef0412aef1e3e9624c7da
Erik <e.ussin@outlook.com>
2018-09-17 03:10:59 +0200

    Neural Network Topology Update

--- src/main/java/com/iis/network/ClassificationNetworkV1.java
+++ src/main/java/com/iis/network/ClassificationNetworkV1.java
@@ -7,15 +7,16 @@ import org.datavec.image.recordreader.ImageRecordReader;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.ConvolutionMode;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.inputs.InputType;
-import org.deeplearning4j.nn.conf.layers.DenseLayer;
-import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.conf.layers.*;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.deeplearning4j.util.ModelSerializer;
+import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.activations.impl.ActivationReLU;
 import org.nd4j.linalg.activations.impl.ActivationSoftmax;
 import org.nd4j.linalg.api.ndarray.INDArray;
@@ -32,28 +33,29 @@ import java.io.File;
 import java.util.Random;
 
 public class ClassificationNetworkV1 {
+    private static final Logger log = LoggerFactory.getLogger(ClassificationNetworkV1.class);
 
-    private static Logger logger = LoggerFactory.getLogger(ClassificationNetworkV1.class);
+    // The width and height of the images
+    private static final int width = 28;
+    private static final int height = 28;
+
+    // Number of (color) channels; in case of grey scale set to 1
+    private static final int channels = 1;
+
+    // Number of classes
+    private static final int numberOfClasses = 30;
+
+    // Hyper parameters
+    private static final int batchSize = 256;
+    private static final int epochs = 20;
+
+    // Reload model and keep training or train new model
+    private static final boolean load_model = false;
 
     public void run() throws Exception {
-
-        // The width and height of the images
-        int width = 28;
-        int height = 28;
-
-        // Number of (color) channels; in case of grey scale set to 1
-        int channels = 1;
-
         // Random seed for predictive results
         int seed = 123;
         Random random = new Random(seed);
-
-        // Hyper parameters
-        int batchSize = 128;
-        int epochs = 15;
-
-        // Number of (image) classes/types in the data set
-        int numberOfClasses = 5;
 
         // Get the data
         File trainingData = new File("data/training");
@@ -79,45 +81,104 @@ public class ClassificationNetworkV1 {
         iterator.setPreProcessor(scaler);
 
         // Build our model
-        logger.info("*** Build model ***");
-        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
-                .seed(seed)
-                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-                .updater(new Nesterovs(0.006, 0.9))
-                .l2(0.0001)
-                .list()
-                .layer(0, new DenseLayer.Builder()
-                        .nIn(height * width)
-                        .nOut(100)
-                        .activation(new ActivationReLU())
-                        .weightInit(WeightInit.XAVIER)
-                        .build()
-                )
-                .layer(1, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
-                        .nIn(100)
-                        .nOut(numberOfClasses)
-                        .activation(new ActivationSoftmax())
-                        .weightInit(WeightInit.XAVIER)
-                        .build()
-                )
-                .pretrain(false)
-                .backprop(true)
-                .setInputType(InputType.convolutional(height, width, channels))
-                .build();
+        MultiLayerNetwork model;
+        log.info("*** Build model ***");
+        if (!load_model){
+            MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                    .seed(seed)
+                    .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                    .updater(new Nesterovs(0.004, 0.8))
+                    .l2(0.0001)
+                    .list()
+                    ///////////
+                    //Layer 1//
+                    ///////////
+                    .layer(0, new ConvolutionLayer.Builder(new int[]{4, 4}, new int[]{1, 1}, new int[]{0, 0})
+                            .convolutionMode(ConvolutionMode.Same)
+                            .nIn(1)
+                            .nOut(64)
+                            .weightInit(WeightInit.XAVIER_UNIFORM)
+                            .activation(Activation.RELU)
+                            .build())
+                    .layer(1, new SubsamplingLayer.Builder(PoolingType.MAX, new int[]{2, 2})
+                            .build())
+                    .layer(2, new DropoutLayer.Builder()
+                            .dropOut(0.99)
+                            .build())
+                    ///////////
+                    //Layer 2//
+                    ///////////
+                    .layer(3, new ConvolutionLayer.Builder(new int[]{3, 3}, new int[]{1, 1}, new int[]{0, 0})
+                            .convolutionMode(ConvolutionMode.Same)
+                            .nOut(96)
+                            .weightInit(WeightInit.XAVIER_UNIFORM)
+                            .activation(Activation.RELU)
+                            .build())
+                    .layer(4, new SubsamplingLayer.Builder(PoolingType.MAX, new int[]{2, 2})
+                            .build())
+                    .layer(5, new DropoutLayer.Builder()
+                            .dropOut(0.99)
+                            .build())
+                    ///////////
+                    //Layer 3//
+                    ///////////
+                    .layer(6, new ConvolutionLayer.Builder(new int[]{2, 2}, new int[]{1, 1}, new int[]{0, 0})
+                            .convolutionMode(ConvolutionMode.Same)
+                            .nOut(128)
+                            .weightInit(WeightInit.XAVIER_UNIFORM)
+                            .activation(Activation.RELU)
+                            .build())
+                    .layer(7, new SubsamplingLayer.Builder(PoolingType.MAX, new int[]{2, 2})
+                            .build())
+                    .layer(8, new DropoutLayer.Builder()
+                            .dropOut(0.99)
+                            .build())
+                    ///////////
+                    //Layer 4//
+                    ///////////
+                    .layer(9, new DenseLayer.Builder()
+                            .nOut(2048)
+                            .activation(new ActivationReLU())
+                            .weightInit(WeightInit.XAVIER)
+                            .build()
+                    )
+                    .layer(10, new DropoutLayer.Builder()
+                            .dropOut(0.95)
+                            .build())
+                    .layer(11, new DenseLayer.Builder()
+                            .nOut(1024)
+                            .activation(new ActivationReLU())
+                            .weightInit(WeightInit.XAVIER)
+                            .build()
+                    )
+                    .layer(12, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                            .nOut(numberOfClasses)
+                            .activation(new ActivationSoftmax())
+                            .weightInit(WeightInit.XAVIER)
+                            .build()
+                    )
+                    .pretrain(false)
+                    .backprop(true)
+                    .setInputType(InputType.convolutional(height, width, channels))
+                    .build();
+            model = new MultiLayerNetwork(conf);
+        } else {
+            model = ModelSerializer.restoreMultiLayerNetwork(new File("trained_qd_model.zip"));
+        }
 
-        MultiLayerNetwork model = new MultiLayerNetwork(conf);
         model.setIterationCount(1);
         model.init();
 
-        model.setListeners(new ScoreIterationListener(10));
+        model.setListeners(new ScoreIterationListener(100));
 
-        logger.info("*** Training model ***");
+        log.info("*** Training model ***");
 
         for (int i = 0; i < epochs; i++) {
             model.fit(iterator);
+            log.info("*** Epoch " + i + " Done ***");
         }
 
-        logger.info("*** Evaluate model ***");
+        log.info("*** Evaluate model ***");
 
         recordReader.reset();
         recordReader.initialize(validation);
@@ -135,9 +196,9 @@ public class ClassificationNetworkV1 {
             evaluation.eval(next.getLabels(), output);
         }
 
-        logger.info(evaluation.stats());
+        log.info(evaluation.stats(true, true));
 
-        logger.info("*** Saving model to 'trained_qd_model.zip' ***");
+        log.info("*** Saving model to 'trained_qd_model.zip' ***");
 
         File saveLocation = new File("trained_qd_model.zip");
         ModelSerializer.writeModel(model, saveLocation, false);
